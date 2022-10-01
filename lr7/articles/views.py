069a42fe6c8f065fae4c00636726b359bd306069
TNBformin <forminlib@gmail.com>
2022-10-01 10:33:44 +0500

    7.7

--- lr7/articles/views.py
+++ lr7/articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
 
 class ArticleListView(ListView):
@@ -12,17 +13,25 @@ class ArticleDetailView(DetailView):
     model = models.Article
     template_name = 'article_detail.html'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Article
     fields = ['title', 'body']
     template_name = 'article_edit.html'
+    login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = models.Article
     template_name = 'article_new.html'
-    fields = ['title','body','author']
+    fields = ['title','body']
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
